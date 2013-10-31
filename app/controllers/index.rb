f0760c615f09bd48a3eee62b3a7a6a3f1ed81eb8
Ryan Briones <ryan.briones@brionesandco.com>
2013-10-31 13:55:02 -0500

    Added a route to increment a counter in the session. Also added a route to clear the session.

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,3 +2,14 @@ get '/' do
   # Look in app/views/index.erb
   erb :index
 end
+
+post '/inc' do
+  session[:counter] ||= 0
+  session[:counter] += 1
+  redirect to('/')
+end
+
+get '/clear' do
+  session.clear
+  redirect to('/')
+end
